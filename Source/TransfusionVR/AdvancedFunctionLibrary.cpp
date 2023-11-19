20abf920506de34a6bcb51d0de9bb4d0cdcd65c6
Will <willcarpenter3@yahoo.com>
2023-11-19 16:52:29 -0500

    Added external imports

---
+++ Source/TransfusionVR/AdvancedFunctionLibrary.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AdvancedFunctionLibrary.h"
+
+#include "Rendering/MultiSizeIndexContainer.h"
+#include "Rendering/SkeletalMeshRenderData.h"
+
+void UAdvancedFunctionLibrary::CopySkeletalMeshToProcedural(USkeletalMeshComponent* SkeletalMeshComponent, int32 LODIndex, UProceduralMeshComponent* ProcMeshComponent)
+{
+	
+}
